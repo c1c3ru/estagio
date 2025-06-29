b451dea84096e2e40b6ebddd72004622e18ee2c5
c1c3ru <cicerosilva.ifce@gmail.com>
2025-06-29 13:41:47 -0300

    feat: aprimora dropdowns na página de registro de estudante
    - Adiciona propriedades de estilo aos dropdowns de turno das aulas e estágio, incluindo cores de fundo e ícones.
    - Melhora a legibilidade do texto nos itens do dropdown, garantindo que a interface seja mais atraente e consistente.

--- lib/features/student/pages/student_register_page.dart
+++ lib/features/student/pages/student_register_page.dart
@@ -317,10 +317,16 @@ class _StudentRegisterPageState extends State<StudentRegisterPage> {
                               'Turno das Aulas', Icons.schedule_outlined),
                           style: const TextStyle(
                               color: AppColors.textPrimaryDark, fontSize: 16),
+                          dropdownColor: AppColors.white,
+                          iconEnabledColor: AppColors.primary,
                           items: ClassShift.values
                               .map((shift) => DropdownMenuItem(
                                     value: shift,
-                                    child: Text(shift.displayName),
+                                    child: Text(
+                                      shift.displayName,
+                                      style: const TextStyle(
+                                          color: AppColors.textPrimaryDark),
+                                    ),
                                   ))
                               .toList(),
                           onChanged: (value) {
@@ -336,10 +342,16 @@ class _StudentRegisterPageState extends State<StudentRegisterPage> {
                               'Turno do Estágio', Icons.work_history_outlined),
                           style: const TextStyle(
                               color: AppColors.textPrimaryDark, fontSize: 16),
+                          dropdownColor: AppColors.white,
+                          iconEnabledColor: AppColors.primary,
                           items: InternshipShift.values
                               .map((shift) => DropdownMenuItem(
                                     value: shift,
-                                    child: Text(shift.displayName),
+                                    child: Text(
+                                      shift.displayName,
+                                      style: const TextStyle(
+                                          color: AppColors.textPrimaryDark),
+                                    ),
                                   ))
                               .toList(),
                           onChanged: (value) {
