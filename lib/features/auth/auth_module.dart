bc0e620cc1e846db799fa094938cb558171ee58a
c1c3ru <cicerosilva.ifce@gmail.com>
2025-08-05 12:15:11 -0300

    feat: adição da página de registro de supervisor e configuração da rota correspondente no módulo de autenticação, utilizando BlocProvider para gerenciar o estado da autenticação

--- lib/features/auth/auth_module.dart
+++ lib/features/auth/auth_module.dart
@@ -5,6 +5,7 @@ import 'package:gestao_de_estagio/features/auth/pages/login_page.dart';
 import 'package:gestao_de_estagio/features/auth/pages/register_type_page.dart';
 import 'package:gestao_de_estagio/features/auth/pages/email_confirmation_page.dart';
 import 'package:gestao_de_estagio/features/auth/pages/forgot_password_page.dart';
+import 'package:gestao_de_estagio/features/auth/pages/supervisor_register_page.dart';
 import 'package:gestao_de_estagio/features/auth/bloc/auth_bloc.dart';
 
 class AuthModule extends Module {
@@ -17,16 +18,29 @@ class AuthModule extends Module {
   void routes(RouteManager r) {
     r.child(
       '/',
-      child: (context) => BlocProvider(
-        create: (_) => Modular.get<AuthBloc>(),
+      child: (context) => BlocProvider.value(
+        value: Modular.get<AuthBloc>(),
         child: const LoginPage(),
       ),
     );
-    r.child('/register', child: (context) => const RegisterTypePage());
-    r.child('/forgot-password', child: (context) => const ForgotPasswordPage());
+    r.child('/register', child: (context) => BlocProvider.value(
+      value: Modular.get<AuthBloc>(),
+      child: const RegisterTypePage(),
+    ));
+    r.child('/forgot-password', child: (context) => BlocProvider.value(
+      value: Modular.get<AuthBloc>(),
+      child: const ForgotPasswordPage(),
+    ));
+    r.child('/register-supervisor', child: (context) => BlocProvider.value(
+      value: Modular.get<AuthBloc>(),
+      child: const SupervisorRegisterPage(),
+    ));
     r.child('/email-confirmation',
-        child: (context) => EmailConfirmationPage(
-              email: r.args.data as String,
-            ));
+        child: (context) => BlocProvider.value(
+          value: Modular.get<AuthBloc>(),
+          child: EmailConfirmationPage(
+            email: r.args.data as String,
+          ),
+        ));
   }
 }
